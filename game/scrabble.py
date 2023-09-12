b1b7f813c86754fa0a4ef86d5cd6fe74a323188d
celi <celi@Ubuntu.myguest.virtualbox.org>
2023-09-11 23:38:27 -0300

    eleventh commit

--- game/scrabble.py
+++ game/scrabble.py
@@ -173,9 +173,26 @@ class Cell:
 class Board:
     def __init__(self):
         self.grid = [
-            [Cell(1,'') for _ in range (15) ]
+            [Cell(1,'letter') for _ in range (15) ]
             for _ in range (15)
         ]
+
+    def validate_word_inside_board(self, word, location, orientation):
+        len_word = len(word)
+        pos_x = location[0]
+        pos_y = location[1]
+
+        if orientation == 'H':
+            if pos_x + len_word > 15:
+                return False
+            else:
+                return True
+        elif orientation == 'V':
+            if pos_y + len_word > 15:
+                return False
+            else:
+                return True
+
 
 class ScrabbleGame:
     def __init__(self, players_count:int):
--- tests/test_board.py
+++ tests/test_board.py
@@ -13,6 +13,23 @@ class TestBoard(unittest.TestCase):
             15,
         )
 
+    def test_word_inside_board_x(self):
+        board = Board()
+        word = 'facultad'
+        location = (5,10)
+        orientation = 'H'
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+        assert word_is_valid == True
+
+
+    def test_word_inside_board_y(self):
+        board = Board()
+        word = 'dragon'
+        location = (13,1)
+        orientation = 'V'
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+        assert word_is_valid == True
+
 
 if __name__ == "__main__":
     unittest.main
