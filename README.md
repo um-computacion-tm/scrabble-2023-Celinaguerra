58f12ecf46f189cdbe55a7e6a0ec90b19bdb6bce
celinaguerra <celinaguerradiaz@gmail.com>
2023-10-11 10:00:41 -0300

    removing circleci develop

--- README.md
+++ README.md
@@ -3,9 +3,6 @@ Author: Celina Guerra Díaz
 Main
 [![CircleCI](https://dl.circleci.com/status-badge/img/gh/um-computacion-tm/scrabble-2023-Celinaguerra/tree/main.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/um-computacion-tm/scrabble-2023-Celinaguerra/tree/main)
 
-Develop
-[![CircleCI](https://dl.circleci.com/status-badge/img/gh/um-computacion-tm/scrabble-2023-Celinaguerra/tree/develop.svg?style=svg)](https://dl.circleci.com/status-badge/redirect/gh/um-computacion-tm/scrabble-2023-Celinaguerra/tree/develop)
-
 [![Maintainability](https://api.codeclimate.com/v1/badges/b0298f893c7928bbb5dc/maintainability)](https://codeclimate.com/github/um-computacion-tm/scrabble-2023-Celinaguerra/maintainability)
 
 [![Test Coverage](https://api.codeclimate.com/v1/badges/b0298f893c7928bbb5dc/test_coverage)](https://codeclimate.com/github/um-computacion-tm/scrabble-2023-Celinaguerra/test_coverage) 
--- game/client.py
+++ game/client.py
@@ -1,20 +1,19 @@
 from game.scrabble import ScrabbleGame
 
-class Main:
-    pass
     # def main():
     #     player_count = int(input('cantidad de jugadores'))
     #     game = ScrabbleGame(player_count)
     #     while(game.playing()):
     #         game...
-"""    def main():
-        player_count = get_player_count()
-        game = ScrabbleGame(player_count)
-        while game.is_playing():
-            show_board(game.get_board())
-            show_player(*game.get_current_player())
-            word, coords, orientation = get_inputs()
-            try:
-                game.play(word, coords, orientation)
-            except Exception as e:
-                print(e)"""
+
+    # def main():
+    #     player_count = get_player_count()
+    #     game = ScrabbleGame(player_count)
+    #     while game.is_playing():
+    #         show_board(game.get_board())
+    #         show_player(*game.get_current_player())
+    #         word, coords, orientation = get_inputs()
+    #         try:
+    #             game.play(word, coords, orientation)
+    #         except Exception as e:
+    #             print(e)
