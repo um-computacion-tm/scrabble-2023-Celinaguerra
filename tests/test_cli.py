9eb35af6e8023a7f2919dcf943f68843f4d7e0c5
celi <celi@Ubuntu.myguest.virtualbox.org>
2023-11-08 14:17:29 -0300

    tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 from game.scrabblecli import *
-from game.board import Board
-from unittest.mock import patch
+from game.board import *
+from game.player import *
+from game.scrabble import *
+from unittest.mock import patch, Mock, MagicMock
 import unittest
 
 
@@ -23,6 +25,23 @@ class TestCLI(unittest.TestCase):
         self.assertEqual(scrabblecli.game.current_status, 'not playing')
 
 
+    @patch('builtins.input', side_effect=1)
+    def test_option_1(self, mock_input):
+        scrabble_game = ScrabbleGame(players_count=2)
+        with patch.object(ScrabbleCli, 'place_word') as mock_place_word:
+            cli = ScrabbleCli()
+            cli.game = scrabble_game
+            cli.option_chosen(1)
+            mock_place_word.assert_called_once()
+
+    @patch('game.scrabble.BagTiles.joker_value')
+    def test_option_2(self, mock_joker_value):
+        scrabble_game = ScrabbleGame(players_count=2)
+        cli = ScrabbleCli()
+        cli.game = scrabble_game
+        cli.option_chosen(2)
+        mock_joker_value.assert_called_once()
+
     @patch('builtins.input', side_effect=3)
     def test_option_3(self, mock_input):
         scrabble_game = ScrabbleGame(players_count=2)
@@ -41,6 +60,24 @@ class TestCLI(unittest.TestCase):
             cli.option_chosen('N')
             mock_exchange_tiles.assert_not_called()
 
+    @patch('builtins.input', side_effect=['4'])
+    def test_option_4(self, mock_input):
+        scrabble_game = ScrabbleGame(players_count=2)
+        cli = ScrabbleCli()
+        cli.game = scrabble_game
+
+        cli.game.next_turn = unittest.mock.MagicMock()
+        cli.option_chosen(4)
+        cli.game.next_turn.assert_called_once()
+
+    @patch('builtins.input', side_effect=5)
+    def test_option_5(self, mock_input):
+        scrabble_game = ScrabbleGame(players_count=2)
+        with patch.object(ScrabbleCli, 'finish_game') as mock_finish_game:
+            cli = ScrabbleCli()
+            cli.game = scrabble_game
+            cli.option_chosen(5)
+            mock_finish_game.assert_called_once()
 
 
 if __name__ == '__main__':
